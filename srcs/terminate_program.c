40588f6e3807196947bd82815728ffabd20bf0e4
Heizo Nakai <hnakai@c1r27s4.42tokyo.jp>
2023-10-03 22:54:29 +0900

    touch okamotosann

--- srcs/terminate_program.c
+++ srcs/terminate_program.c
@@ -6,7 +6,7 @@
 /*   By: hnakai <hnakai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:08:15 by hnakai            #+#    #+#             */
-/*   Updated: 2023/10/03 22:46:05 by hnakai           ###   ########.fr       */
+/*   Updated: 2023/10/03 22:53:16 by hnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,5 @@ void	terminate_program(int signum, siginfo_t *pid, void *context)
 	printf("%d\n", pid->si_pid);
 	kill(pid->si_pid, SIGUSR1);
 }
+
+//test
