cce7e6b9edf713c47bb0f5d4b79115be19fc88a5
42hnakai <n.heizoudesu@gmail.com>
2023-10-19 21:37:47 +0900

    modify my_cd

--- srcs/signal/terminate_program.c
+++ srcs/signal/terminate_program.c
@@ -6,7 +6,7 @@
 /*   By: nakaiheizou <nakaiheizou@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:08:15 by hnakai            #+#    #+#             */
-/*   Updated: 2023/10/16 18:40:22 by nakaiheizou      ###   ########.fr       */
+/*   Updated: 2023/10/19 21:35:42 by nakaiheizou      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,26 @@
 
 bool	signal_received = false;
 
-void	signal_handler(int signum);
+void	put_new_prompt(int signum);
+void	nothing_to_do(int signum);
 
 bool	is_signal_received(void)
 {
 	signal_received = false;
-	signal(SIGINT, signal_handler);
+	signal(SIGINT, put_new_prompt);
+	signal(SIGQUIT, nothing_to_do);
 	return (signal_received);
 }
 
-void	signal_handler(int signum)
+void	put_new_prompt(int signum)
 {
 	signal_received = true;
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+}
+
+void	nothing_to_do(int signum)
+{
+	;
 }
