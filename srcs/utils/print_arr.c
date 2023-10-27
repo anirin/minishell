fc1081618e69672c2bf1689e4e8b36cc70d3a01b
42hnakai <n.heizoudesu@gmail.com>
2023-10-27 17:59:26 +0900

    finish norm_check utils

--- srcs/utils/print_arr.c
+++ srcs/utils/print_arr.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_arr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nakaiheizou <nakaiheizou@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 17:53:10 by nakaiheizou       #+#    #+#             */
+/*   Updated: 2023/10/27 17:57:10 by nakaiheizou      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "main.h"
 
-void print_arr(char **arr)
+void	print_arr(char **arr)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	printf("--print arr--\n");
-	while(arr[i] != NULL)
+	while (arr[i] != NULL)
 	{
 		printf("%d : [%s]\n", i, arr[i]);
 		i++;
